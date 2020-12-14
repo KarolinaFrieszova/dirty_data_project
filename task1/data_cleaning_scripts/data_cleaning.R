eff4cceb562812a9012401dfc069421d5d9184b8
Karolina Frieszova <karolinafriesz@gmail.com>
2020-12-14 12:25:58 +0000

    clean data & three analytical observations

--- task1/data_cleaning_scripts/data_cleaning.R
+++ task1/data_cleaning_scripts/data_cleaning.R
@@ -1,2 +1,23 @@
 library(tidyverse)
 
+decathlon <- read_rds("raw_data/decathlon.rds")
+
+# convert row names of a data frame into a variable
+
+decathlon <- tibble::rownames_to_column(decathlon, "athlete")
+
+# rename columns
+
+names(decathlon) <- c("athlete", "sprint_100m", "long_jump", "shot_put", "high_jump", 
+                      "sprint_400m", "hurdle_110m", "discus", "pole_vault", "javeline", 
+                      "distance_1500m", "rank", "points", "competition")
+
+# formatting
+
+decathlon <- decathlon %>% 
+  mutate(athlete = str_to_title(athlete),
+         athlete = str_replace(athlete, "Mcmullen", "McMullen"),
+         competition = str_replace_all(competition, "OlympicG", "Olympic_Games"))
+
+write_rds(decathlon, "clean_data/decathlon.rds")
+
